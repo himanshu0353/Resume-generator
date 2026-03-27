b5d65d24340a1bf0a613be82c40dfa92e2b5a4af
ashishsingh-beep <ashish.singh@wildnetedge.com>
2026-03-28 01:48:27 +0530

    structure all folder and integrate frontend-backend

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,12 +1,21 @@
 const jwt = require('jsonwebtoken')
+const TokenBlacklistModel  = require('../models/blacklist.model')
 
-function authUser(req, res, next){
+async function authUser(req, res, next){
     const token = req.cookies.token
     if(!token){
         return res.status(401).json({
             message: "Token not provided"
         })
     }
+
+    const isTokenBlacklisted = await TokenBlacklistModel.findOne({token})
+    if(isTokenBlacklisted){
+        return res.status(401).json({
+            message: "Token is invalid"
+        })
+    }
+
     try{
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
     req.user = decoded
